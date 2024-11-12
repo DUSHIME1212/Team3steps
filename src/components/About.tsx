b8cc4052098765274cae489cfa81dc817a51f88d
honorine22 <higiranez@gmail.com>
2024-11-12 14:19:30 +0200

    make who we are responsive

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import React from "react";
 import { IoPersonCircleOutline } from "react-icons/io5";
@@ -11,47 +12,49 @@ const About = () => {
   const img3 =
     "https://d33wubrfki0l68.cloudfront.net/8a595f4793ed112a9a00389b1c99eb538e67eb39/84a3b/assets/images/3.jpg";
   return (
-    <div className="min-h-96 p-8 md:px-16 flex lg:px-32">
-      <div className="w-1/2 p-16">
-        <div className=" mb-4 flex flex-col gap-4">
-          <span className="font-semibold text-blue-500">WHO ARE WE</span>
-          <h1 className="text-5xl font-semibold">
-            Assisting individuals in locating the appropriate real estate.
-          </h1>
-          <p className="text-sm">
-            Donec porttitor euismod dignissim. Nullam a lacinia ipsum, nec
-            dignissim purus. Nulla convallis ipsum molestie nibh malesuada, ac
-            malesuada leo volutpat.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4">
-          <div className="flex gap-4 items-center p-4 flex-row">
-            <TbSmartHome size={52} />
-            <div>
-              <span className="font-semibold text-blue-500">OUR MISSION</span>
-              <p>Nullam a lacinia ipsum, nec dignissim purus. Nulla</p>
+    <div className="min-h-full md:min-h-96 lg:min-h-96 p-8 md:px-16 flex lg:px-32">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+        <div>
+          <div className="mb-4 flex flex-col gap-4">
+            <span className="font-semibold text-blue-500">WHO ARE WE</span>
+            <h1 className="text-2xl md:text-5xl lg:text-5xl font-semibold">
+              Assisting individuals in locating the appropriate real estate.
+            </h1>
+            <p className="text-sm">
+              Donec porttitor euismod dignissim. Nullam a lacinia ipsum, nec
+              dignissim purus. Nulla convallis ipsum molestie nibh malesuada, ac
+              malesuada leo volutpat.
+            </p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <div className="flex gap-4 items-center p-4 flex-row">
+              <TbSmartHome size={52} />
+              <div>
+                <span className="font-semibold text-blue-500">OUR MISSION</span>
+                <p>Nullam a lacinia ipsum, nec dignissim purus. Nulla</p>
+              </div>
+            </div>
+            <div className="flex gap-4 items-center p-4 flex-row">
+              <IoPersonCircleOutline size={52} />
+              <div>
+                <span className="font-semibold text-blue-500">
+                  Donec porttitor euismod
+                </span>
+                <p>Nullam a lacinia ipsum, nec dignissim purus. Nulla</p>
+              </div>
             </div>
           </div>
-          <div className="flex gap-4 items-center p-4 flex-row">
-            <IoPersonCircleOutline size={52} />
-            <div>
-              <span className="font-semibold text-blue-500">
-                Donec porttitor euismod
-              </span>
-              <p>Nullam a lacinia ipsum, nec dignissim purus. Nulla</p>
-            </div>
+        </div>
+        <div className="grid grid-rows-3 md:grid-rows-2 lg:grid-rows-2 gap-x-4 gap-y-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+          <div className="row-span-1 md:row-span-2 lg:row-span-2 relative rounded-3xl overflow-clip">
+            <Image src={img1} alt="image" className="object-cover" fill />
           </div>
-        </div>
-      </div>
-      <div className="w-1/2 p-16 grid grid-rows-2 gap-4 grid-cols-2">
-        <div className="row-span-2 relative rounded-3xl overflow-clip">
-          <Image src={img1} alt="" className="object-cover" fill />
-        </div>
-        <div className="relative rounded-3xl overflow-clip">
-          <Image src={img2} alt="" className="object-cover" fill />
-        </div>
-        <div className="relative rounded-3xl overflow-clip">
-          <Image src={img3} alt="" className="object-cover" fill />
+          <div className="relative rounded-3xl overflow-clip">
+            <img src={img2} alt="image" className="object-cover" />
+          </div>
+          <div className="relative rounded-3xl overflow-clip">
+            <img src={img3} alt="image" className="object-cover" />
+          </div>
         </div>
       </div>
     </div>
